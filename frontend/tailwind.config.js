322aa5df8af148326cdd975785aec98fc2eb7da5
SumeeraMadushanka <sumeeramadushanka104@gmail.com>
2022-05-24 02:40:16 +0530

    package install

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'bg-homepage': "url(../src/components/assets/bg.jpg)"
+        'bg-homepage': "url(../src/components/assets/bg.jpg)",
+        'bg-create': "url(../src/components/assets/sales-2.jpg)"
       }
     },
   },
